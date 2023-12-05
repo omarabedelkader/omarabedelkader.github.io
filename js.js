f575ed88c3394ae9b0b1ab8e8309e499e01a1146
omarabedelkader <omarabedelkader1@gmail.com>
2023-12-05 20:10:23 +0100

    update

---
+++ js.js
@@ -0,0 +1,13 @@
+var mywebsitename = "OMAR ABEDELKADER's Webiste"
+
+var myName = "Omar ABEDELKADER";
+
+var mypositionfr = "Data Scientist - Chercheur TAL" 
+
+var mypositionen = "Data Scientist - NLP Researcher" 
+
+var aboutfr = "Un innovateur analytique axé sur les résultats, codeur multilingue et architecte en intelligence artificielle avec une expertise particulière en traitement du langage naturel et en mathématiques appliquées."
+
+var abouten = "A results-oriented analytical innovator, multilingual coder, and AI architect with specialized knowledge in Natural Language Processing and Applied Mathematics"
+
+document.title = myName;
